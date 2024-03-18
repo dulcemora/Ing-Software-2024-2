13233779bee95a5bac6880fe64c56c118d20bc3b
dulcemora <dulcemora03@ciencias.unam.mx>
2024-03-17 19:25:41 -0600

    Agregando mas borrar usuario

--- Practica03/contollers/ControllerUsuario.py
+++ Practica03/contollers/ControllerUsuario.py
@@ -41,8 +41,32 @@ def agregar_usuario():
         return "Usuario agregado"
         #return redirect(url_for('usuario.ver_usuarios'))
 
-@usuario_blueprint.route('/borrar',  methods=['GET', 'POST'])
+@usuario_blueprint.route('/borrar_usuario', methods=['GET', 'POST'])
 def borrar_usuario():
-    #borra_usuario(1)
-    #print("Borrado con éxito!")
-    return "borrar"
+    if request.method == 'GET':
+        return render_template('Usuario/borrar_usuario.html')
+    else:
+        idUsuario = request.form['idUsuario']
+        # Puedes manejar otros campos como la imagen de perfil y si es superusuario aquí
+        usuario_a_eliminar = Usuario.query.get_or_404(idUsuario)
+        #nuevo_usuario = Usuario(nombre=nombre, password=password, email=email)
+        if usuario_a_eliminar:
+            db.session.delete(usuario_a_eliminar)
+            db.session.commit()
+            return 'Usuario eliminado'
+        else:
+            return 'Usuario no encontrado', 404
+        flash('Usuario eliminado correctamente', 'success')
+        return render_template('Usuario/borrar_usuario.html')
+        #return redirect(url_for('usuario.ver_usuarios'))
+
+'''
+@usuario_blueprint.route('/id/<int:id_usuario>/<string:nombre>')
+@usuario_blueprint.route('/borrar_usuario',  methods=['POST'])
+def borrar_usuario():
+    idUsuario = request.form['idUsuario']
+    usuario = Usuario.query.filter(Usuario.idUsuario).first()
+    db.session.delete(usuario)
+    db.session.commit()
+    return render_template('Usuario/borrar_usuario.html')
+'''
